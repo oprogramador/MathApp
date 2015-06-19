6ed47621823ab83d7ed39ed11beba24e075ff443
oprogramador <uomodislesia@gmail.com>
2015-06-19 06:52:55 +0200

    tests

--- js/Test.js
+++ js/Test.js
@@ -11,16 +11,22 @@ var Test;
     function MyObject() {
         function test(moduleName) {
             var testModuleName = moduleName+'_Test';
+            var runNr = 0;
+            var failedNr = 0;
             Require.reload(moduleName);
             Require.reload(testModuleName, {path: 'test', callback: function() {
                 var testModule = eval(testModuleName);
                 for(var i in testModule) {
+                    runNr++;
                     try {
                         testModule[i]();
                     } catch(e) {
+                        failedNr++;
                         console.error(testModule[i].name+' '+e.message);
                     }
                 }
+                console.log('Run '+runNr+' tests.');
+                console.log('Failed '+failedNr+' tests.');
             }});
         }
 
--- test/ObjectUtils_Test.js
+++ test/ObjectUtils_Test.js
@@ -14,7 +14,22 @@ var ObjectUtils_Test;
             Assert.assert(JSON.stringify(x) === JSON.stringify(y));
         }
 
+        function isReallyCopied() {
+            var x = {a:1, b:2};
+            var y = ObjectUtils.clone(x);
+            y.a++;
+            Assert.assert(y.a === 2);
+            Assert.assert(x.a === 1);
+        }
+
+        function isNotDeepCloned() {
+            var x = {a: {a:1, b:2}};
+            var y = ObjectUtils.clone(x);
+            y.a.a++;
+        }
+
         this.isWellCloned = isWellCloned;
+        this.isReallyCopied = isReallyCopied;
     }
 
     ObjectUtils_Test = new MyObject();
